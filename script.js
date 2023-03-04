dacd3d4f87c05d737cea60fe77b4b9563a0627a1
Steve Amos <somaevets@gmail.com>
2023-03-04 09:49:09 -0600

    Establish boilerplate code for particle system

--- script.js
+++ script.js
@@ -5,4 +5,40 @@ canvas.height = window.innerHeight;
 
 //canvas settings
 ctx.fillStyle = "#f1f1f1";
-console.log(ctx);
+ctx.strokeStyle = '#f1f1f1'
+ctx.lineWidth = 1;
+
+class Particle{
+  constructor(effect) {
+    this.effect = effect;
+    this.x = Math.floor(Math.random() * this.effect.width);
+    this.y = Math.floor(Math.random() * this.effect.height);
+  }
+  draw(context) {
+    context.fillRect(this.x,this.y, 10, 10);
+  }
+}
+
+class Effect{
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.particles = [];
+    this.numberOfParticles = 50;
+  }
+  init() {
+    for (let i = 0; i < this.numberOfParticles; i++){
+      this.particles.push(new Particle(this)); 
+    }
+  }
+  render(context) {
+    this.particles.forEach(particle => {
+      particle.draw(context);
+    });
+  }
+}
+
+const effect = new Effect(canvas.width, canvas.height);
+effect.init();
+effect.render(ctx);
+console.log(effect);
