8a46d5c5c5c3a72fa7b1fb46459f7d50bdc69670
Steve Amos <somaevets@gmail.com>
2023-03-04 15:51:49 -0600

    Max length to line to control length.

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ class Particle{
     this.speedX = Math.random() * 5 - 2.5;
     this.speedY = Math.random() * 5 - 2.5;
     this.history = [{x: this.x, y: this.y}];
+    this.maxLength = Math.floor(Math.random() * 100 + 10);
+    this.angle = 0;
   }
   draw(context) {
     context.fillRect(this.x,this.y, 5, 5);
@@ -30,6 +32,9 @@ class Particle{
     this.x += this.speedX + Math.random()* 15 - 7.5;
     this.y += this.speedY + Math.random()* 15 - 7.5;
     this.history.push({x: this.x, y: this.y});
+    if (this.history.length > this.maxLength) {
+      this.history.shift();
+    }
   }
 }
 
