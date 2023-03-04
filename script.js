af0c550cfcf947d6826e2e3d2d0c862c0c07c590
Steve Amos <somaevets@gmail.com>
2023-03-03 19:54:52 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+//canvas settings
+ctx.fillStyle = "#f1f1f1";
+console.log(ctx);
