8e84d456f540c30e9f816e746a419d5ed3f5d02d
Steve Amos <somaevets@gmail.com>
2023-03-04 16:03:53 -0600

    introducing trigonometry for circles and swiggles.

--- script.js
+++ script.js
@@ -29,8 +29,9 @@ class Particle{
     context.stroke();
   }
   update() {
-    this.x += this.speedX + Math.random()* 15 - 7.5;
-    this.y += this.speedY + Math.random()* 15 - 7.5;
+    this.angle += 0.25;
+    this.x += this.speedX + Math.sin(this.angle) * 3; //change combinations of values
+    this.y += this.speedY + Math.cos(this.angle) * 2; //
     this.history.push({x: this.x, y: this.y});
     if (this.history.length > this.maxLength) {
       this.history.shift();
